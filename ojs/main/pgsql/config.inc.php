f2674e99925e96d0a869b60faa6a21ebffe57cf7
pkp-machine-user <withanage+1921992@users.noreply.github.com>
2026-03-08 06:39:21 +0000

    Update datasets ( ojs main pgsql)

--- ojs/main/pgsql/config.inc.php
+++ ojs/main/pgsql/config.inc.php
@@ -26,7 +26,7 @@
 
 ; An application-specific key that is required for the app to run
 ; Internally this is used for any encryption (specifically cookie encryption if enabled)
-app_key = "base64:PvALuuqfMZyOlZyPb1OQxQFin3RNbxedOd0h08HLWRQ="
+app_key = "base64:35Xx7QxUs7ttSq7LewirGKoQ4z7KZQPszmNS+pvYZOY="
 
 ; Set this to On once the system has been installed
 ; (This is generally done automatically by the installer)
